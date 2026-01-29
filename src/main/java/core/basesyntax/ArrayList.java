fc723d7a043f97e384ac2a08332e8e159199d35c
Alex <ursusandwolf@protonmail.com>
2026-01-29 14:30:01 +0200

    add(T value) in end of list

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,9 +1,27 @@
 package core.basesyntax;
 
 public class ArrayList<T> implements List<T> {
-    @Override
-    public void add(T value) {
+    public static final int DEFAULT_CAPACITY = 10;
+    private int capacity = DEFAULT_CAPACITY;
+    private int size = 0;
+    private T[] storage;
 
+    @SuppressWarnings("unchecked")
+    public ArrayList() {
+        storage = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void add(T value) {
+        if (size >= capacity) {
+            T[] temp = storage;
+            capacity = (int) (1.5 * capacity);
+            storage = (T[]) new Object[capacity];
+            System.arraycopy(temp, 0, storage, 0, size);
+        }
+        storage[size] = value;
+        size++;
     }
 
     @Override
@@ -18,7 +36,10 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException(index + " is OutOfBounds");
+        }
+        return storage[index];
     }
 
     @Override
@@ -38,11 +59,11 @@ public class ArrayList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
